c83b2e6abc58b6f7031cd8d95d2d52c8d64ea7ca
MyKe01 <mstasi01@gmail.com>
2024-04-20 17:26:37 +0200

    modified homography calculation and real points calculation - real time positions estimation and collecting centers when players are static

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import threading
 import mediapipe as mp
 import time
 
-def computePoseAndAnkles(cropped_frame, ankles_queue,  mpPose, pose, mpDraw, homography_matrix_inv, prev_right_ankle, prev_left_ankle, threshold):
+def computePoseAndAnkles(cropped_frame, static_centers_queue,  mpPose, pose, mpDraw, hom_matrix, prev_right_ankle, prev_left_ankle, threshold, x_offset, y_offset, rect_img):
 
     imgRGB = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
@@ -20,17 +20,33 @@ def computePoseAndAnkles(cropped_frame, ankles_queue,  mpPose, pose, mpDraw, hom
             h, w,c = cropped_frame.shape
             #print(id, lm)
             if id == 28 : 
-                right_ankle= (int(lm.x*w), int(lm.y*h))
+                right_ankle = (int(lm.x*w), int(lm.y*h))
                 cv2.circle(cropped_frame, right_ankle, 5, (0,0,255), cv2.FILLED)
-                Pright_image = cv2.perspectiveTransform(np.array([[right_ankle]], dtype=np.float32), homography_matrix_inv)[0][0]                         # Computation of the field coordinates of the left ankle using H^(-1)   (I believe the values are not correct, so modifications are needed)
-                Pright_image = (round(Pright_image[0]),round(Pright_image[1]))                                                                       # Approximation to avoid displaying all the decimals
-                cv2.putText(cropped_frame, f"{Pright_image}", (right_ankle[0] + 10, right_ankle [1]), font, font_scale, color, thickness, cv2.LINE_AA)        # Display of the left foot field coordinates values at image coordinates, with slight offset on the X axis to avoid overlapping with the actual foot
+
+                #right_ankle but in the context of the full frame (not the cropped one)
+                right_ankle_real = (right_ankle[0] + x_offset, right_ankle[1] + y_offset, 0)
+                right_ankle_real = np.array([[right_ankle_real[0], right_ankle_real[1]]], dtype=np.float32)
+                right_ankle_real = np.reshape(right_ankle_real, (1,1,2))
+                #standard function to get the resulting point by applying the homography to the point of the image
+                Pright_image = cv2.perspectiveTransform(right_ankle_real, hom_matrix)                     
+                # Approximation to avoid displaying all the decimals      
+                Pright_image = (round(Pright_image[0][0][0]), round(Pright_image[0][0][1]))
+                # Display of the right foot field real coordinates values at image coordinates, with slight offset on the X axis to avoid overlapping with the actual foot
+                cv2.putText(cropped_frame, f"{Pright_image}", (right_ankle[0] + 10, right_ankle [1]), font, font_scale, color, thickness, cv2.LINE_AA)        
             elif id == 27 :
                 left_ankle = (int(lm.x*w), int(lm.y*h))
                 cv2.circle(cropped_frame, left_ankle, 5, (0,255,0), cv2.FILLED)
-                Pleft_image = cv2.perspectiveTransform(np.array([[left_ankle]], dtype=np.float32), homography_matrix_inv)[0][0]                         # Computation of the field coordinates of the left ankle using H^(-1)   (I believe the values are not correct, so modifications are needed)
-                Pleft_image = (round(Pleft_image[0]),round(Pleft_image[1]))                                                                       # Approximation to avoid displaying all the decimals
-                cv2.putText(cropped_frame, f"{Pleft_image}", (left_ankle[0] + 10, left_ankle [1] + 20), font, font_scale, color, thickness, cv2.LINE_AA)        # Display of the left foot field coordinates values at image coordinates, with slight offset on the X axis to avoid overlapping with the actual foot
+                
+                #left_ankle but in the context of the full frame (not the cropped one)
+                left_ankle_real = (left_ankle[0] + x_offset, left_ankle[1] + y_offset, 0)
+                left_ankle_real = np.array([[left_ankle_real[0], left_ankle_real[1]]], dtype=np.float32)
+                left_ankle_real = np.reshape(left_ankle_real, (1,1,2))
+                #standard function to get the resulting point by applying the homography to the point of the image
+                Pleft_image = cv2.perspectiveTransform(left_ankle_real, hom_matrix)
+                # Approximation to avoid displaying all the decimals      
+                Pleft_image = (round(Pleft_image[0][0][0]),round(Pleft_image[0][0][1]))
+                # Display of the left foot field real coordinates values at image coordinates, with slight offset on the X axis to avoid overlapping with the actual foot
+                cv2.putText(cropped_frame, f"{Pleft_image}", (left_ankle[0] + 10, left_ankle [1] + 20), font, font_scale, color, thickness, cv2.LINE_AA)
             else :
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 cv2.circle(cropped_frame, (cx, cy), 5, (255,0,0), cv2.FILLED)
@@ -58,9 +74,15 @@ def computePoseAndAnkles(cropped_frame, ankles_queue,  mpPose, pose, mpDraw, hom
     prev_right_ankle[0] = Pright_image[0]
     prev_right_ankle[1] = Pright_image[1]
     
+    #computing the center position of the player in the real field
+    center_real = tuple((int((Pright_image[0] + Pleft_image[0])/2), int((Pright_image[1] + Pleft_image[1])/2)))  
+    
+    #collecting static positions
     if right_ankle != (0,0) and left_ankle != (0,0) and left_foot_moved != True and right_foot_moved != True : 
-        ankles_queue.append((right_ankle, left_ankle))
-
+        static_centers_queue.append(center_real)
+    #displaying live positions of the player
+    center_real = (round(center_real[0]), round(center_real[1]))
+    cv2.circle(rect_img, center_real , 5, (255, 255, 0), cv2.FILLED)
 
 def select_points(image):
     # Display the image and allow the user to select points
@@ -115,8 +137,9 @@ def calculate_homography(image_points, field_points, field_length, field_width):
     # |           |
     # |           |
     # D-----------C
-
-    field_corners = np.array([[0, field_length], [field_width, field_length], [field_width, 0], [0, 0]], dtype=np.float32)
+    #offset to move the origin 
+    offset = 150
+    field_corners = np.array([[offset + 0, offset + 0], [offset + field_width, offset +  0], [offset + field_width, offset +  field_length],  [offset + 0,offset +  field_length]], dtype=np.float32)
 
     # Calculate the homography
     homography, _ = cv2.findHomography(image_points, field_corners)
@@ -129,7 +152,7 @@ field_length = 23.78 #meters
 field_width = 10.97 #meters
 
 #we need a scale factor since the sizes are in meters and if scale_factor = 1 the returned image will be really small
-scale_factor = 30
+scale_factor = 20
 field_length *=scale_factor
 field_width *=scale_factor
 
@@ -149,11 +172,17 @@ image_with_lines = draw_lines(image, points)
 homography_matrix = calculate_homography(points, points, field_length, field_width)
 homography_matrix_inv = np.linalg.inv(homography_matrix)
 
-"""
+""" testing purpose
 # Rectify the original image using the calculated homography
 rectified_image = cv2.warpPerspective(image, homography_matrix, (image.shape[1], image.shape[0]))
 # Display the rectified image to check the correctness of the homography matrix 
 plt.imshow(rectified_image)
+# Define a point in the original image (x, y)
+point = np.array([[390, 200]], dtype=np.float32)
+# Reshape the point array to shape (1, 1, 2)
+point = np.reshape(point, (1, 1, 2))
+point =cv2.perspectiveTransform(point, homography_matrix)
+plt.plot(point[0][0][0], point[0][0][1], 'ro')
 plt.title("Rectified Image")
 plt.show()
 # Display the image with selected points and lines
@@ -183,11 +212,6 @@ threshold_moving = 5
 # Loading of the clip to analyze
 cap = cv2.VideoCapture("resources/tennis2.mp4")
 
-# Allocation to write the resulting evaluation in a video file at the end
-result = cv2.VideoWriter('result2.mp4',
-                         cv2.VideoWriter_fourcc(*'mp4v'),
-                         60, (1280, 720))
-
 mpPose_A = mp.solutions.pose
 pose_A = mpPose_A.Pose()
 mpDraw_A = mp.solutions.drawing_utils
@@ -199,8 +223,21 @@ mpDraw_B = mp.solutions.drawing_utils
 # PROCESSING LOOP
 # each landmark has an id - https://developers.google.com/mediapipe/solutions/vision/pose_landmarker
 # ids 28 and 27 are for right and left ankle
-ankles_queue_A = list()
-ankles_queue_B = list()
+# lists of the static points of the two players (A -> DOWN , B -> UP)
+stationary_points_A = list()
+stationary_points_B = list()
+
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #set the color from BGR to RGB
+
+#changing the rectified image to "clean" it from the previous drawings of the center
+rectified_image = cv2.warpPerspective(image, homography_matrix, (image.shape[1], image.shape[0]))
+
+# Allocation to write the resulting evaluation in a video file at the end
+# Maybe width has to be changed : TODO
+result = cv2.VideoWriter('result2.mp4',
+                         cv2.VideoWriter_fourcc(*'mp4v'),
+                         60, (image.shape[1] + rectified_image.shape[1], 720))
 
 while cv2.waitKey(1) < 0:
     hasFrame, frame = cap.read()
@@ -208,11 +245,15 @@ while cv2.waitKey(1) < 0:
         # cv2.waitKey()
         break
     cTime = time.time()
+    
+    #changing the rectified image to "clean" it from the previous drawings of the center
+    rectified_image = cv2.warpPerspective(image, homography_matrix, (image.shape[1], image.shape[0]))
 
     cropped_frame_B = frame[90:250, 390:855].copy()
     cropped_frame_A = frame[400:720, 100:1100].copy()
-    th_A = threading.Thread(target=computePoseAndAnkles, args=(cropped_frame_A, ankles_queue_A, mpPose_A, pose_A, mpDraw_A, homography_matrix_inv, prev_PrightA_image, prev_PleftA_image, threshold_moving))
-    th_B = threading.Thread(target=computePoseAndAnkles, args=(cropped_frame_B, ankles_queue_B,  mpPose_B, pose_B, mpDraw_B, homography_matrix_inv, prev_PrightB_image, prev_PleftB_image, threshold_moving))
+    #creating two threads to improve performances for the detection of the pose
+    th_A = threading.Thread(target=computePoseAndAnkles, args=( cropped_frame_A, stationary_points_A, mpPose_A, pose_A, mpDraw_A, homography_matrix, prev_PrightA_image, prev_PleftA_image, threshold_moving, 100, 400, rectified_image))
+    th_B = threading.Thread(target=computePoseAndAnkles, args=( cropped_frame_B, stationary_points_B, mpPose_B, pose_B, mpDraw_B, homography_matrix, prev_PrightB_image, prev_PleftB_image, threshold_moving, 390,  90, rectified_image))
     
     th_A.start()
     th_B.start()
@@ -223,15 +264,21 @@ while cv2.waitKey(1) < 0:
     fps = 1/(cTime-pTime)
     frame[400:720, 100:1100] = cropped_frame_A
     frame[90:250, 390:855] = cropped_frame_B
-
     cv2.putText(frame, str(int(fps)), (50,50), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 3)
-    cv2.imshow("Image", frame) 
-    result.write(frame)
+    #combining the two images 
+    height = max(frame.shape[0], rectified_image.shape[0])
+
+    frame = cv2.resize(frame, (int(frame.shape[1] * height / frame.shape[0]), height))
+    rectified_image = cv2.resize(rectified_image, (int(rectified_image.shape[1] * height / rectified_image.shape[0]), height))
+    rectified_image = cv2.cvtColor(rectified_image, cv2.COLOR_RGB2BGR)
+
+    combined_image = cv2.hconcat([frame, rectified_image])
+    cv2.imshow('Combined Images', combined_image)
+    result.write(combined_image)
 
 
 cap.release()
 result.release()
-
 # Closes all the frames
 cv2.destroyAllWindows()
 
